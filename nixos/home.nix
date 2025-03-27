be5a8a1d4662e6d72160be209759612ca38c76cc
Sean <smiller2000@live.com>
2025-03-26 17:46:17 -0700

    move around some files

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,26 +1,21 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  home.username = "sean";
-  home.homeDirectory = "/home/sean";
 
-  home.stateVersion = "24.11";
+  imports = [ ../modules/home-manager/vscode.nix ];
 
-  imports = [ ./vscode.nix ];
-
-  home.packages = [
-    pkgs.nixd
-    pkgs.nil
-    pkgs.nixfmt-rfc-style
+  home.packages = with pkgs; [
+    nixd
+    nil
+    nixfmt-rfc-style
   ];
 
-  home.file = {
-
+  home = {
+    username = "sean";
+    homeDirectory = "/home/sean";
   };
 
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
+  home.stateVersion = "24.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
