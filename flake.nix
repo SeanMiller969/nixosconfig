3d6fa92dfb03c8a2c0f5c4981d6dd8b33ca51cd4
Sean <smiller2000@live.com>
2025-03-28 20:26:09 -0700

    update vscode extensions to follow my nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,12 +2,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    nix-vscode-extensions.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
